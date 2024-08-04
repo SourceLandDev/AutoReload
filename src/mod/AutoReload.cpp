9e9899bc588dbd4aae1a185c23f785f292a9446b
予纾 <100036558+Yuisyuu@users.noreply.github.com>
2024-08-04 16:02:14 +0800

    update

--- src/mod/AutoReload.cpp
+++ src/mod/AutoReload.cpp
@@ -1,9 +1,8 @@
-#include "plugin/AutoReload.h"
+#include "mod/AutoReload.h"
 
 #include <memory>
 
-#include "ll/api/plugin/NativePlugin.h"
-#include "ll/api/plugin/RegisterHelper.h"
+#include "ll/api/mod/RegisterHelper.h"
 
 namespace auto_reload {
 
@@ -12,17 +11,20 @@ static std::unique_ptr<AutoReload> instance;
 AutoReload& AutoReload::getInstance() { return *instance; }
 
 bool AutoReload::load() {
+    // Code for loading the mod goes here.
     return true;
 }
 
 bool AutoReload::enable() {
+    // Code for enabling the mod goes here.
     return true;
 }
 
 bool AutoReload::disable() {
+    // Code for disabling the mod goes here.
     return true;
 }
 
 } // namespace auto_reload
 
-LL_REGISTER_PLUGIN(auto_reload::AutoReload, auto_reload::instance);
+LL_REGISTER_MOD(auto_reload::AutoReload, auto_reload::instance);
--- src/plugin/MemoryOperators.cpp
+++
@@ -1,6 +0,0 @@
-// This file will make your plugin use LeviLamina's memory operators by default.
-// This improves the memory management of your plugin and is recommended to use.
-
-#define LL_MEMORY_OPERATORS
-
-#include "ll/api/memory/MemoryOperators.h" // IWYU pragma: keep
